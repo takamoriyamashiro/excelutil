3654317d0c0278735b3a231f669a21b19b936df3
Takamori Yamashiro <297174+takamoriyamashiro@users.noreply.github.com>
2018-07-15 22:27:44 +0900

    add function getCellName

--- src/ExcelUtil.php
+++ src/ExcelUtil.php
@@ -5,9 +5,60 @@ namespace takamoriyamashiro\ExcelUtil;
 class ExcelUtil
 {
 	
+	const INDEXES = [
+		'A' => 1,
+		'B' => 2,
+		'C' => 3,
+		'D' => 4,
+		'E' => 5,
+		
+		'F' => 6,
+		'G' => 7,
+		'H' => 8,
+		'I' => 9,
+		'J' => 10,
+		
+		'K' => 11,
+		'L' => 12,
+		'M' => 13,
+		'N' => 14,
+		'O' => 15,
+		
+		'P' => 16,
+		'Q' => 17,
+		'R' => 18,
+		'S' => 19,
+		'T' => 20,
+		'U' => 21,
+		'V' => 22,
+		'W' => 23,
+		'X' => 24,
+		'Y' => 25,
+		'Z' => 26
+	];
 	
+	public static function getIndex($cellName)
+	{
+		
+		
+	}
 	
+	public static function getCellName($index)
+	{
+		$result = [];
+		if($index==0){
+			return false;
+		}
+		while($index > 0){
+			$remainder = ($index % 26) ?: 26;
+			array_unshift($result, self::get1place($remainder));
+			$index = ($index - $remainder) / 26;
+		}
+		return implode('',$result);
+	}
 	
-	
+	private static function get1place($remainder){
+		return chr($remainder + 64);
+	}
 	
 }
--- test/ExcelUtilTest.php
+++ test/ExcelUtilTest.php
@@ -14,11 +14,19 @@ class ExcelUtilTest extends TestCase
 		
 	}
 	
+	
+	
 	public function testColumnName()
 	{
+		$this->assertTrue(ExcelUtil::getCellName('10')=='J');
+		$this->assertTrue(ExcelUtil::getCellName(28)=='AB');
+		$this->assertTrue(ExcelUtil::getCellName(128)=='DX');
+		$this->assertFalse(ExcelUtil::getCellName(0));
 		
-		
-		$this->assertTrue(true);
+		echo "\n";
+		for($index=1;$index<100;$index++){
+			echo $index.":".ExcelUtil::getCellName($index)."\n";
+		}
 		
 	}
 	
